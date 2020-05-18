12157930c9ed919a196bf44c4769b565b419d9a4
ar78ilnik <arakchiev.ilya@yandex.ru>
2020-05-18 22:01:57 +0300

    Card mobile

--- app/js/toogle.js
+++ app/js/toogle.js
@@ -3,6 +3,7 @@
 var navMain = document.querySelector(".list-nav");
 var navToggle = document.querySelector(".button__nav");
 var header = document.querySelector(".header");
+var menuCatalog = document.querySelector(".list_menu-catalog");
 
 
 navToggle.addEventListener("click", function () {
@@ -10,9 +11,11 @@ navToggle.addEventListener("click", function () {
     navMain.classList.add("list-nav_opened");
     navMain.classList.remove("list-nav_closed");
     header.classList.add("header_opened");
+    menuCatalog.classList.remove("list_menu-catalog-closed");
   } else {
     navMain.classList.add("list-nav_closed");
     navMain.classList.remove("list-nav_opened");
     header.classList.remove("header_opened");
+    menuCatalog.classList.add("list_menu-catalog-closed");
   }
 });
