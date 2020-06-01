108a7e1a41638534a015230ec1b7bf0c73caddcc
ar78ilnik <arakchiev.ilya@yandex.ru>
2020-06-01 23:06:40 +0300

    Menu last

--- app/js/toogle.js
+++ app/js/toogle.js
@@ -1,21 +1,12 @@
 "use strict";
 
-var navMain = document.querySelector(".list-nav");
 var navToggle = document.querySelector(".button__nav");
 var header = document.querySelector(".header");
-var menuCatalog = document.querySelector(".list_menu-catalog");
-
 
 navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("list-nav_closed")) {
-    navMain.classList.add("list-nav_opened");
-    navMain.classList.remove("list-nav_closed");
-    header.classList.add("header_opened");
-    menuCatalog.classList.remove("list_menu-catalog-closed");
-  } else {
-    navMain.classList.add("list-nav_closed");
-    navMain.classList.remove("list-nav_opened");
+  if (header.classList.contains("header_opened")) {
     header.classList.remove("header_opened");
-    menuCatalog.classList.add("list_menu-catalog-closed");
+  } else {
+    header.classList.add("header_opened");
   }
 });
